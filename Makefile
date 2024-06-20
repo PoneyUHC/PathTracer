5195227fbd1c36cd08cf4341abe4d85582350dbd
PoneyUHC <com_lor@laposte.net>
2024-06-20 12:56:36 +0200

    C++17 needed for cross-platform filesystem operations

--- Makefile
+++ Makefile
@@ -4,16 +4,16 @@ EXEC_NAME = render.exe
 OUTPUT_DIR = output
 SRC_DIR = src
 SRC = $(wildcard $(SRC_DIR)/*.cpp) $(wildcard $(SRC_DIR)/**/*.cpp)
-FLAGS = -g -O0
+CFLAGS = -g -O0 -Wall -std=c++17
 
 default: render.exe
 
 $(EXEC_NAME):
 	echo $(SRC)
 	if [ ! -d $(BUILD_DIR) ]; then mkdir $(BUILD_DIR); fi
-	g++ $(SRC) $(FLAGS) -o $(BUILD_DIR)/$(EXEC_NAME)
+	g++ $(SRC) $(CFLAGS) -o $(BUILD_DIR)/$(EXEC_NAME)
 
 run:
 	if [ ! -d $(BUILD_DIR) ]; then mkdir $(BUILD_DIR); fi
-	g++ $(SRC) $(FLAGS) -o $(BUILD_DIR)/$(EXEC_NAME)
+	g++ $(SRC) $(CFLAGS) -o $(BUILD_DIR)/$(EXEC_NAME)
 	$(BUILD_DIR)/$(EXEC_NAME) $(ARGS)
--- src/exporter/ppm.cpp
+++ src/exporter/ppm.cpp
@@ -1,18 +1,24 @@
 
 #include "ppm.hpp"
 
+#include "../utils.hpp"
 
-#include <string>
+#include <filesystem>
 #include <iostream>
 #include <fstream>
 
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int export_ppm(std::string filepath, int width, int height, RGBColor *buffer)
 {
-    // TODO: create boost dependency and use boost library
-    system("mkdir ./output/");
+    fs::path path(filepath);
+    fs::path parentDir = path.parent_path();
+    if( !fs::exists(parentDir) ){
+        fs::create_directory(parentDir);
+    }
+    
     ofstream file(filepath);
     if ( !file.is_open()) {
         cerr << __FUNCTION__ << " : failed to open " << filepath << endl;
