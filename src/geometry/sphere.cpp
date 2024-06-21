c51f022300ee7fae48818e0c6a0bece2817ec16c
PoneyUHC <com_lor@laposte.net>
2024-06-21 15:35:36 +0200

    simplification of calcul in sphere-ray intersection

--- src/geometry/sphere.cpp
+++ src/geometry/sphere.cpp
@@ -13,16 +13,17 @@ std::optional<Point3> Sphere::HitPoint(const Ray& ray)
     const Vec3 d = ray.Direction();
     const Vec3 qc = m_center - q;
     const double a = d.Dot(d);
-    const double b = -2 * d.Dot(qc);
-    const double c = qc.Dot(qc) - m_radius*m_radius;
+    // b = -2*h for simplified calculation
+    const double h = d.Dot(qc);
+    const double c = qc.MagnitudeSquared() - m_radius*m_radius;
 
-    const double discriminant = b*b - 4*a*c;
+    const double discriminant = h*h - a*c;
 
     if(discriminant < 0){
         return std::nullopt;
     } 
     
-    const double firstRoot = (-b - sqrt(discriminant)) / 2*a;
+    const double firstRoot = (h - sqrt(discriminant)) / a;
 
     Point3 hitPoint = ray.At(firstRoot);
     return hitPoint;
