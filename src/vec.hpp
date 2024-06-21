d871848f497e83aaa3505752e0242e1ccc7f87ad
PoneyUHC <com_lor@laposte.net>
2024-06-21 14:39:48 +0200

    FIX monkey dot product

--- src/vec.hpp
+++ src/vec.hpp
@@ -23,10 +23,14 @@ public:
     Vec3 operator/(double n) const { return Vec3{e[0]/n, e[1]/n, e[2]/n}; }
     Vec3 operator+(const Vec3& other) const { return Vec3{e[0]+other.e[0], e[1]+other.e[1], e[2]+other.e[2]}; }
 
-    inline Vec3 Dot(const Vec3& other) { return Vec3{e[0]*other.e[0], e[1]*other.e[1], e[2]*other.e[2]}; }
+    inline double Dot(const Vec3& other) const { return e[0]*other.e[0] + e[1]*other.e[1] + e[2]*other.e[2]; }
     inline double Magnitude() const { return std::sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
     inline Vec3 Normalized() const { return *this / this->Magnitude(); }
 };
+
+
+Vec3 operator*(double n, const Vec3& u) { return Vec3{n*u.e[0], n*u.e[1], n*u.e[2]}; }
+
 
 typedef Vec3 Point3;
 typedef Vec3 RGBColor;
