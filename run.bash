aa380cabd6be37bb90ff1d414fc27f44b9f0d434
PoneyUHC <uhc.contact@laposte.net>
2024-07-29 21:59:55 +0200

    fix run.bash opening image before execution

--- run.bash
+++ run.bash
@@ -1,9 +1,7 @@
 
 make render.exe
 result=$?
-if [ "$result" -eq "0" ]; then
-    code ./output/render.ppm
-else
+if [ "$result" -ne "0" ]; then
     echo -e "\033[0;31mErrors during compilation\033[0m"
     exit
 fi
