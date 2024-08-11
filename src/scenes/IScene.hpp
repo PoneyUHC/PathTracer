560661187f7962d6c61f628680964b2d369dbdce
PoneyUHC <uhc.contact@laposte.net>
2024-08-11 23:19:02 +0200

    class for scene building  management

---
+++ src/scenes/IScene.hpp
@@ -0,0 +1,38 @@
+
+#pragma once
+
+#include "math/vec.hpp"
+#include "renderer/IRenderer.hpp"
+
+#include <memory>
+
+
+struct SceneParams {
+    uint32_t render_width;
+    bool enable_bvh;
+};
+
+
+class HittableList;
+class Camera;
+class IRenderer;
+
+class IScene {
+
+protected:
+
+    SceneParams m_params;
+    std::shared_ptr<HittableList> m_objets;
+    std::shared_ptr<Camera> m_camera;
+    std::shared_ptr<IRenderer> m_renderer;
+
+
+public:
+
+    ~IScene() = default;
+    virtual int Build(SceneParams&& params) = 0;
+    virtual std::shared_ptr<RGBColor[]> Render() = 0;
+
+    const Camera* GetCamera() { return m_camera.get(); }
+
+};
